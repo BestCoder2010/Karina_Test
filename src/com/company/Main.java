627f9bb26605c375c9eea4192b82ec76554e8806
Norik <noriksaroyan@gmail.com>
2022-07-26 00:17:01 +0300

    Мой первый проект!!!!!!!!!!!!!

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+  String colour = "red ";
+  String song = "jjj";
+
+ System.out.println ("colour: " + colour );
+ System.out.println ("song: " + song);
+
+//
+//        String name = "Karina";
+//        String surname = "Lange";
+//        int age = 12;
+//
+//        System.out.println("name: " + name);
+//        System.out.println("surname:" + surname);
+//        System.out.println("age: " + age);
+////        System.out.println(surname);
+
+
+    }
+}
